2ae4eb6e08c9c1be207808f4a315254837084d24
Hannah Cherb <hannahlcherb@gmail.com>
2023-01-18 19:08:50 -0600

    I made an output change.

--- practiceWithGitHub.cpp
+++ practiceWithGitHub.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World! This is my GitHub practice!\n";
+    std::cout << "Hello World! This is my GitHub practice! Let's make a change now.\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
